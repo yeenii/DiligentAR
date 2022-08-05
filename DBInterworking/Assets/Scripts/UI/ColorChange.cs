a7ea24f85c6687aa351fe902626a1a19fe121ae2
yeenii <jye0042@naver.com>
2022-08-05 21:18:47 +0900

    change
    UI 수정

--- DBInterworking/Assets/Scripts/UI/ColorChange.cs
+++ DBInterworking/Assets/Scripts/UI/ColorChange.cs
@@ -12,7 +12,6 @@ public class ColorChange : MonoBehaviour
     public GameObject[] gameObjects;
 
 
-    //int i = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -30,7 +29,7 @@ public class ColorChange : MonoBehaviour
     void ValueChanged(string text) // string 매개변수는 기본으로 들어가는 매개변수이다
     {
        
-        Debug.Log(text.Length+ " - 글자 길이");
+        //Debug.Log(text.Length+ " - 글자 길이");
 
         gameObjects[0].SetActive(false);
         gameObjects[1].SetActive(false);
@@ -47,51 +46,5 @@ public class ColorChange : MonoBehaviour
         
     }
 
-
-    //void Function()
-    //{
-    //    MyString = text;
-    //    result.text = MyString;
-    //}
-
-    //void ChangeColor() // string 매개변수는 기본으로 들어가는 매개변수이다
-    //{
-    //    if (result.Legth <= 1)
-    //    {
-    //        gameObjects[0].SetActive(true);
-    //        gameObjects[1].SetActive(false);
-    //        gameObjects[2].SetActive(false);
-    //        gameObjects[3].SetActive(false);
-    //    }
-    //    else if (result.Length <= 2)
-    //    {
-    //        gameObjects[0].SetActive(true);
-    //        gameObjects[1].SetActive(true);
-    //        gameObjects[2].SetActive(false);
-    //        gameObjects[3].SetActive(false);
-    //    }
-    //    else if (result.Length <= 3)
-    //    {
-    //        gameObjects[0].SetActive(true);
-    //        gameObjects[1].SetActive(true);
-    //        gameObjects[2].SetActive(true);
-    //        gameObjects[3].SetActive(false);
-
-    //    }
-    //    else if (MyString.Length <= 4)
-    //    {
-    //        gameObjects[0].SetActive(true);
-    //        gameObjects[1].SetActive(true);
-    //        gameObjects[2].SetActive(true);
-    //        gameObjects[3].SetActive(true);
-    //    }
-
-
-    //    Debug.Log(text + text.Length + " - 글자 입력 중");
-
-
-    //    i++;
-
-    //}
 
 }
